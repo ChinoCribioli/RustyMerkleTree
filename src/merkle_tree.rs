5dfa7b97d128ba0dd57b6fe8e3ce5d8841087a1f
ChinoCribioli <ezequiel.cribioli@gmail.com>
2024-03-29 17:07:25 -0300

    deleted unnecessary clones

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -22,18 +22,19 @@ pub struct MerkleTree<T> where T: Clone, T: Hash {
     values: Vec<Option<T>>,
 }
 
-impl<T: Clone + Hash> MerkleTree<T> {
+impl<T: Clone + Copy + Hash> MerkleTree<T> {
     
     pub fn new(values: Vec<T>) -> MerkleTree<T> {
         let mut option_values: Vec<Option<T>> = Vec::new();
         
         // We populate the self.values array, padding it to reach a power of 2 length
-        for value in values.iter() {
-            option_values.push(Some(value.clone()));
+        let values_len = values.len();
+        for value in values {
+            option_values.push(Some(value));
         }
         let mut pow2: usize = 1;
-        while pow2 < values.len() { pow2 *= 2; }
-        option_values.append(&mut vec![None; pow2 - option_values.len()]);
+        while pow2 < values_len { pow2 *= 2; }
+        option_values.append(&mut vec![None; pow2 - values_len]);
         assert_eq!(option_values.len(), pow2);
 
         let mut nodes = Vec::new();
@@ -44,7 +45,7 @@ impl<T: Clone + Hash> MerkleTree<T> {
                 left: None,
                 right: None,
             };
-            nodes.push(Box::new(node.clone()));
+            nodes.push(Box::new(node));
         }
         
         while nodes.len() > 1 {
@@ -54,11 +55,11 @@ impl<T: Clone + Hash> MerkleTree<T> {
                 let right_node = nodes[index+1].clone();
                 let new_node: Node = Node {
                     node_hash: hash_values(vec![left_node.node_hash, right_node.node_hash]),
-                    interval: (left_node.interval.0,right_node.interval.1),
-                    left: Some(left_node.clone()),
-                    right: Some(right_node.clone()),
+                    interval: (left_node.interval.0, right_node.interval.1),
+                    left: Some(left_node),
+                    right: Some(right_node),
                 };
-                new_level.push(Box::new(new_node.clone()));
+                new_level.push(Box::new(new_node));
             };
             nodes = new_level;
         }
@@ -68,15 +69,15 @@ impl<T: Clone + Hash> MerkleTree<T> {
         }
     }
 
-    pub fn get_root_hash(self) -> u64 {
+    pub fn get_root_hash(&self) -> u64 {
         self.root.node_hash
     }
 
-    pub fn in_range(index: usize, interval: (usize, usize)) -> bool {
+    fn in_range(index: usize, interval: (usize, usize)) -> bool {
         interval.0 <= index && index <= interval.1
     }
 
-    fn check_index_range(self, index: usize) {
+    fn check_index_range(&self, index: usize) {
         if index >= self.values.len() {
             panic!("Index out of range of the tree.");
         }
@@ -86,44 +87,52 @@ impl<T: Clone + Hash> MerkleTree<T> {
         if node.interval.0 == node.interval.1 {
             return Vec::new();
         };
-        if Self::in_range(index, node.left.clone().expect("Node without left child").interval) {
+        let left_ref = node.left.as_ref().expect("Found node without left child when getting proof");
+        let right_ref = node.right.as_ref().expect("Found node without right child when getting proof");
+        if Self::in_range(index, left_ref.interval) {
             let mut previous_path = Self::get_recursive_path(*node.left.unwrap(), index);
-            previous_path.push(node.right.unwrap().node_hash);
-            return previous_path;
-        } else {
+            previous_path.push(right_ref.node_hash);
+            previous_path
+        } else if Self::in_range(index, right_ref.interval) {
             let mut previous_path = Self::get_recursive_path(*node.right.unwrap(), index);
-            previous_path.push(node.left.unwrap().node_hash);
-            return previous_path;
+            previous_path.push(left_ref.node_hash);
+            previous_path
+        } else {
+            panic!("Proof took the incorrect path when getting proof.")
         }
     }
     
     pub fn get_with_proof(self, index: usize) -> (T, Vec<u64>) {
-        self.clone().check_index_range(index);
-        let value = self.values[index].clone().expect("There is no value at that index");
+        self.check_index_range(index);
+        let value = self.values[index].as_ref().expect("There is no value at that index");
         let path = Self::get_recursive_path(*self.root, index);
-        (value, path)
+        (*value, path)
     }
 
-    fn recalculate_hashes(self, node: &mut Node, index: usize) {
+    fn recalculate_hashes(node: &mut Node, index: usize, new_value: T) {
         if node.interval.0 == node.interval.1 {
-            node.node_hash = hash_values(vec![&self.values[index]]);
+            node.node_hash = hash_values(vec![Some(new_value)]);
             return;
         };
-        if Self::in_range(index, node.clone().left.expect("Node without left child").interval) {
-            self.recalculate_hashes(node.left.as_mut().unwrap(), index);
+        let left_ref = node.left.as_ref().expect("Found node without left child when recalculating hashes");
+        let right_ref = node.right.as_ref().expect("Found node without right child when recalculating hashes");
+        if Self::in_range(index, left_ref.interval) {
+            Self::recalculate_hashes(node.left.as_mut().unwrap(), index, new_value);
+        } else if Self::in_range(index, right_ref.interval) {
+            Self::recalculate_hashes(node.right.as_mut().unwrap(), index, new_value);
         } else {
-            self.recalculate_hashes(node.right.as_mut().unwrap(), index);
+            panic!("Proof took the incorrect path when recalculating hashes.")
         }
         node.node_hash = hash_values(vec![
-            node.clone().left.unwrap().node_hash,
-            node.clone().right.unwrap().node_hash
+            node.left.as_ref().unwrap().node_hash,
+            node.right.as_ref().unwrap().node_hash
         ]);
     }
 
     pub fn change_value(&mut self, index: usize, new_value: T) -> u64 {
-        self.clone().check_index_range(index);
+        self.check_index_range(index);
         self.values[index] = Some(new_value);
-        self.clone().recalculate_hashes(self.root.as_mut(), index);
+        Self::recalculate_hashes(self.root.as_mut(), index, new_value);
         self.root.node_hash
     }
 
